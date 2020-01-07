44711db1d7e07cf1c9d97c7c5146002fa86a759f
Thananchai Pinyo <porgariso123@gmail.com>
2020-01-07 10:26:33 +0700

    Initial Commit

---
+++ ios_test_job/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_test_job
+//
+//  Created by Thananchai Pinyo on 1/7/20.
+//  Copyright © 2020 Thananchai Pinyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios_test_jobTests/ios_test_jobTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_test_jobTests.swift
+//  ios_test_jobTests
+//
+//  Created by Thananchai Pinyo on 1/7/20.
+//  Copyright © 2020 Thananchai Pinyo. All rights reserved.
+//
+
+import XCTest
+@testable import ios_test_job
+
+class ios_test_jobTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
